190d9b2b2330ed157b70131978d830ac91829411
Balazs Gibizer <gibi@redhat.com>
2022-04-22 12:01:13 +0200

    Use proper sed separator for paths
    I941ef5ea90970a0901236afe81c551aaf24ac1d8 added a sed command that
    should match and delete path values but used '/' as sed separator. This
    leads to error in unstack.sh runs when the path also contains '/':
    
    +./unstack.sh:main:188 sudo sed -i '/directory=/opt/stack/ d' /etc/gitconfig
    sed: -e expression #1, char 13: unknown command: `o'
    
    So this patch replace '/' separator with '+'.
    
    Change-Id: I06811c0d9ee7ecddf84ef1c6dd6cff5129dbf4b1

--- unstack.sh
+++ unstack.sh
@@ -195,4 +195,4 @@ fi
 # Clean any safe.directory items we wrote into the global
 # gitconfig. We can identify the relevant ones by checking that they
 # point to somewhere in our $DEST directory.
-sudo sed -i "/directory=${DEST}/ d" /etc/gitconfig
+sudo sed -i "\+directory = ${DEST}+ d" /etc/gitconfig
